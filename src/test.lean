80f297dac0149bd99fbf2763a104bd9eed0902dc
potsrevennil <potsrevennil@gmail.com>
2018-11-04 18:06:41 +0800

    Remove redundant code

--- src/test.lean
+++ src/test.lean
@@ -36,9 +36,9 @@ begin
   end,
 end
 
-lemma mul_mod_lem {a b : ℕ} : ∀ (x : ℤ), a * x ≡ a * (x % b) [ZMOD b] := 
+lemma mul_mod_lem {b : ℕ} : ∀ (a x : ℤ), a * x ≡ a * (x % b) [ZMOD b] := 
 begin
-  intro,
+  intros,
   let h₁ : x ≡ (x % b) [ZMOD b],
   begin
     apply int.modeq.symm,
@@ -47,6 +47,17 @@ begin
   let h₂ := int.modeq.modeq_mul_left,
   swap, exact b, swap, exact x, swap, exact (x % b), 
   apply (h₂ a h₁)
+end
+
+lemma int_coe_to_nat_eq : ∀ (x : ℤ), (x > 0) → (↑(int.to_nat x) = x) :=
+begin
+  intros x h₀,
+  cases x,
+  let h₁ : int.to_nat (int.of_nat x) = x,
+  refl, rw h₁ at *, refl,
+  let h₂ := int.neg_succ_lt_zero x,
+  let h₃ := not_lt_of_le (le_of_lt h₂),
+  apply (absurd h₀ h₃)
 end
 
 def pf {p : ℕ} := {e : ℤ// prime p ∧ 0 ≤ e ∧ e < p}
@@ -101,63 +112,21 @@ begin
     end
 end
 
-lemma pf_inv_trans_lem₁ {a b : ℕ} {h₁ : b > 0} (h₂ : ∃ (x : ℤ), a * x ≡ 1 [ZMOD b]) : (∃ (x : ℤ), (x ≥ 0) ∧ x < b ∧ a * x ≡ 1 [ZMOD b]) := 
-begin
-  apply exists.elim h₂,
-  assume x, assume fx,
-    
-  apply exists.intro,
-  swap,
-  exact (x % b),
-  let h := mod_range h₁ x,
-  apply and.intro, apply h.left,
-  apply and.intro, apply h.right,
-  begin
-    apply int.modeq.trans, apply int.modeq.symm,
-    apply mul_mod_lem,
-    assumption
-  end
-end
-
-lemma pf_inv_trans_lem₂ {a : ℕ} {h₁ : prime p} (h₂ : ∃ (x : ℤ), (x ≥ 0) ∧ x < p ∧ a * x ≡ 1 [ZMOD p]) : (∃ (x : @pf p), a * x.1 ≡ 1 [ZMOD p]) :=
-begin
-  apply exists.elim h₂,
-  assume x, assume h₃,
-  apply exists.intro,
-  swap, exact (@int_to_pf p x h₁), rw int_to_pf, simp,
-  apply int.modeq.trans, apply int.modeq.symm,
-  apply mul_mod_lem,
-  apply h₃.right.right
-end
-
 theorem inv_exist (e : @pf p) (h₁ : e.1 > 0) : (∃ (x : @pf p), e.1 * x.1 ≡ 1 [ZMOD p]) :=
   begin
     let h₂ := int.modeq.mod_coprime ((pf_coprime e) h₁),
     cases e, simp at *, simp at *,
-    let h₃ := pf_inv_trans_lem₁,
-    swap, exact (int.to_nat e_val), swap, exact p,
     begin
-      let h₄ := pf_inv_trans_lem₂,
-      swap, exact p, swap, exact (int.to_nat e_val),
-      let h₄₁ : ↑(int.to_nat e_val) = e_val,
-      begin
-        cases e_val,
-        let h₄₂ : int.to_nat (int.of_nat e_val) = e_val,
-        refl, rw h₄₂ at *, refl,
-        let h₃₁ := int.neg_succ_lt_zero e_val,
-        let h₃₂ := not_lt_of_le (le_of_lt h₃₁),
-        apply (absurd h₁ h₃₂)
-      end,
-      let h₅ := (h₄ (h₃ h₂)),
-      rw h₄₁ at *,
-      assumption,
-      apply e_property.left
-    end,
-    begin
-        apply (prime.pos e_property.left)
-    end,
+      apply exists.elim h₂,
+      intros x h₃,
+      apply exists.intro,
+      swap, exact (@int_to_pf p x e_property.left), rw int_to_pf, simp,
+      apply int.modeq.trans, apply int.modeq.symm,
+      apply mul_mod_lem,
+      rw int_coe_to_nat_eq at *,
+      apply h₃, repeat {try {exact h₁}},
+    end
   end
-
 
 end pf
 
