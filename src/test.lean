9db4ed95c650927272dadf2927afc7e2e968a5e7
petercommand <petercommand@gmail.com>
2018-11-20 15:15:05 +0800

    implement mono_order_lem

--- src/test.lean
+++ src/test.lean
@@ -1,6 +1,7 @@
 import data.finset
 import linear_algebra.multivariate_polynomial
 import ring_theory.ideals
+import tactic.find
 variables {α : Type*} {β : Type*} {γ : Type*} {δ : Type*} {ι : Type*}
 variables [comm_semiring α]
 variables [decidable_eq α]
@@ -15,6 +16,7 @@ namespace finsupp
 
 lemma in_not (a : ℕ →₀ ℕ) : ∀ x : ℕ, x ∈ a.support ∨ x ∉ a.support := 
     by intro; apply classical.or_not
+
 
 def le_aux : (ℕ →₀ ℕ) → (ℕ →₀ ℕ) → ℕ → Prop
 | a b 0 := a 0 ≤ b 0
@@ -36,6 +38,29 @@ begin
     apply (absurd h₁ (not_lt_of_ge h₃))
 end
 
+lemma gt_sup_eq_zero (a : ℕ →₀ ℕ) : ∀ k : ℕ, max a < k → a k = 0 :=
+begin
+    intros,
+    have h : k ∉ a.support := gt_max_not_in a k a_1,
+    have h' := in_not a k,
+    cases h', exact absurd h' h,
+    apply not_mem_support_iff.elim_left, assumption
+end
+
+lemma max_ab_ge_max_a : ∀ (a b : ℕ →₀ ℕ), max_ab a b ≥ max a :=
+begin
+    intros a b,
+    unfold max_ab max,
+    rw finset.sup_union, apply lattice.le_sup_left,
+end
+
+lemma max_ab_ge_max_b : ∀ (a b : ℕ →₀ ℕ), max_ab a b ≥ max b :=
+begin
+    intros a b,
+    unfold max_ab max,
+    rw finset.sup_union, apply lattice.le_sup_right,
+end
+
 lemma gt_max_ab_not_in : ∀ (a b : ℕ →₀ ℕ) (x : ℕ), (x > max_ab a b) → x ∉ a.support ∧ x ∉ b.support :=
 begin
     intros a b x h₁,
@@ -51,14 +76,16 @@ begin
     apply (gt_max_not_in b x h₅ h₂),
 end
 
-lemma le_of_succ_eq (a b : ℕ →₀ ℕ) (i  : ℕ) (hlei : le_aux a b i) (hi_succ : a (i + 1) = b (i + 1)) : le_aux a b (i + 1) :=
+lemma le_of_succ_eq (a b : ℕ →₀ ℕ) (i  : ℕ) (hlei : le_aux a b i) 
+      (hi_succ : a (i + 1) = b (i + 1)) : le_aux a b (i + 1) :=
 begin
     unfold le_aux,
     apply or.inr, apply and.intro,
     repeat {assumption},
 end
 
-lemma le_of_succ_eqs (a b : ℕ →₀ ℕ) (i j : ℕ) (hlei : le_aux a b i) (hk : ∀ k : ℕ, i < k → k ≤ (i + j) → a k = b k) : le_aux a b (i + j) :=
+lemma le_of_succ_eqs (a b : ℕ →₀ ℕ) (i j : ℕ) (hlei : le_aux a b i) 
+    (hk : ∀ k : ℕ, i < k → k ≤ (i + j) → a k = b k) : le_aux a b (i + j) :=
 begin
     revert i,
     induction j,
@@ -76,7 +103,6 @@ begin
         assumption, refl,
     end,
 end
-
 lemma le_add (a b w : ℕ →₀ ℕ) (i : ℕ) (hab : le_aux a b i) : le_aux (a + w) (b + w) i :=
 begin
     induction i,
@@ -91,27 +117,17 @@ lemma mono_order_lem : ∀ (a b w : ℕ →₀ ℕ), (a ≤ b) → ((a + w) ≤ 
 begin 
     intros a b w hab,
     unfold has_le.le finsupp.le at *,
-    have htri := lt_trichotomy (max_ab a b) (max_ab (a + w) (b + w)),
-    cases htri,
-    begin
-        cases (max_ab (a + w) (b + w)),
-        cases htri,
-        unfold le_aux at *, simp, apply or.inr,
-    end,
-    cases htri,
-    begin
-        
-        
-    end,
-    
-
-    /-
-    all_goals {cases (max_ab a b); cases (max_ab (a + w) (b + w))}, 
-    cases h, cases h,
-    unfold le_aux at *, 
-    
-    simp at *,
-    -/
+    apply le_add,
+    have h := le_of_succ_eqs a b (max_ab a b) ((max_ab (a + w) (b + w)) - (max_ab a b)) hab _ ,
+    rw nat.add_sub_of_le at h, assumption,
+    swap,
+    intros,
+    rw nat.add_sub_of_le at a_2,
+    rw [gt_sup_eq_zero a k, gt_sup_eq_zero b k],
+    have h' := max_ab_ge_max_b a b,
+    apply nat.lt_of_le_of_lt h' a_1,
+    have h' := max_ab_ge_max_a a b,
+    apply nat.lt_of_le_of_lt h' a_1,
 end
 
 instance : is_monomial_order (ℕ →₀ ℕ) finsupp.le := sorry
