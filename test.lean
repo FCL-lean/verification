abad8f7a1f2a7ebd9c13de1e94501f3f3db022b6
Han <potsrevennil@gmail.com>
2018-11-20 14:07:20 +0800

    Proving monomial order...

---
+++ test.lean
@@ -0,0 +1,135 @@
+import data.finset
+import linear_algebra.multivariate_polynomial
+import ring_theory.ideals
+variables {α : Type*} {β : Type*} {γ : Type*} {δ : Type*} {ι : Type*}
+variables [comm_semiring α]
+variables [decidable_eq α]
+variables [comm_ring (mv_polynomial ℕ α)]
+
+variable (ss : set (mv_polynomial ℕ α))
+
+class is_monomial_order (α : Type*) (r : α → α → Prop) extends has_add α, is_linear_order α r :=
+    (mono_order : ∀ a b w : α, (r a b) → r (a + w) (b + w) )
+
+namespace finsupp
+
+lemma in_not (a : ℕ →₀ ℕ) : ∀ x : ℕ, x ∈ a.support ∨ x ∉ a.support := 
+    by intro; apply classical.or_not
+
+def le_aux : (ℕ →₀ ℕ) → (ℕ →₀ ℕ) → ℕ → Prop
+| a b 0 := a 0 ≤ b 0
+| a b (nat.succ m) := (a (nat.succ m) < b (nat.succ m)) ∨ 
+                        ((a (nat.succ m) = b (nat.succ m)) ∧ le_aux a b m) 
+    
+def max (a : ℕ →₀ ℕ) : ℕ := a.support.sup id
+def max_ab (a b : ℕ →₀ ℕ) : ℕ := (a.support ∪ b.support).sup id
+protected def le : (ℕ →₀ ℕ) → (ℕ →₀ ℕ) → Prop := λ a b, le_aux a b $ max_ab a b
+
+/-
+inductive less_than_or_equal (a b : ℕ →₀ ℕ) : ℕ → Prop
+| zero : a 0 ≤ b 0 → less_than_or_equal 0
+| succ_le : ∀ m : ℕ, a (nat.succ m) < b (nat.succ m) → less_than_or_equal (nat.succ m)
+| succ_eq : ∀ m : ℕ, a (nat.succ m) = b (nat.succ m) → less_than_or_equal m → less_than_or_equal (nat.succ m)
+
+protected def le (a b : ℕ →₀ ℕ) := finsupp.less_than_or_equal a b $ max a b
+-/
+
+instance : has_le (ℕ →₀ ℕ) := ⟨finsupp.le⟩ 
+
+lemma gt_max_not_in : ∀ (a : ℕ →₀ ℕ) (x : ℕ), (x > max a) → x ∉ a.support :=
+begin
+    intros a x h₁ h₂,
+    unfold max at *,
+    let h₃ : id x ≤ finset.sup (a.support) id := finset.le_sup h₂,
+    rw id at *,
+    apply (absurd h₁ (not_lt_of_ge h₃))
+end
+
+lemma gt_max_ab_not_in : ∀ (a b : ℕ →₀ ℕ) (x : ℕ), (x > max_ab a b) → x ∉ a.support ∧ x ∉ b.support :=
+begin
+    intros a b x h₁,
+    unfold max_ab at *, 
+    let h₃ : finset.sup (a.support ∪ b.support) id = finset.sup (a.support) id ⊔ finset.sup (b.support) id :=
+  finset.sup_union,
+    rw h₃ at *,
+    have h₄ : finset.sup (a.support) id < x := lt_of_le_of_lt lattice.le_sup_left h₁,
+    have h₅ : finset.sup (b.support) id < x := lt_of_le_of_lt lattice.le_sup_right h₁,
+    apply and.intro, 
+    all_goals {intro h₂},
+    apply (gt_max_not_in a x h₄ h₂),
+    apply (gt_max_not_in b x h₅ h₂),
+end
+
+lemma le_of_succ_eq (a b : ℕ →₀ ℕ) (i  : ℕ) (hlei : le_aux a b i) (hi_succ : a (i + 1) = b (i + 1)) : le_aux a b (i + 1) :=
+begin
+    unfold le_aux,
+    apply or.inr, apply and.intro,
+    apply (not_lt_of_ge (le_of_eq hi_succ)),
+    assumption,
+end
+
+lemma le_of_succ_eqs (a b : ℕ →₀ ℕ) (i j : ℕ) (hlei : le_aux a b i) (hk : ∀ k : ℕ, i < k → k ≤ (i + j) → a k = b k) : le_aux a b (i + j) :=
+begin
+    revert i,
+    induction j,
+    begin
+        intros i hlei hk, assumption,
+    end,
+    begin
+        intros i hlei hk,
+        apply le_of_succ_eq,
+        apply j_ih, assumption,
+        intros k hik hki,
+        apply (hk k hik (nat.le_succ_of_le hki)),
+        apply hk,
+        let h : i < i + nat.succ j_n := nat.lt_add_of_pos_right (nat.zero_lt_succ j_n),
+        assumption, refl,
+    end,
+end
+
+lemma le_add (a b w : ℕ →₀ ℕ) (i : ℕ) (hab : le_aux a b i) : le_aux (a + w) (b + w) i :=
+begin
+    induction i,
+    all_goals {unfold le_aux at *, simp},
+    assumption,
+    cases hab, left, assumption,
+    cases hab, apply or.inr, apply and.intro, assumption,
+    apply (i_ih hab_right),
+end
+
+
+lemma mono_order_lem : ∀ (a b w : ℕ →₀ ℕ), (a ≤ b) → ((a + w) ≤ (b + w)) := 
+begin 
+    intros a b w hab,
+    unfold has_le.le finsupp.le at *,
+    have htri := lt_trichotomy (max_ab a b) (max_ab (a + w) (b + w)),
+    cases htri,
+    begin
+        cases (max_ab (a + w) (b + w)),
+        cases htri,
+        unfold le_aux at *, simp, apply or.inr,
+    end,
+    cases htri,
+    begin
+        
+        
+    end,
+    
+
+    /-
+    all_goals {cases (max_ab a b); cases (max_ab (a + w) (b + w))}, 
+    cases h, cases h,
+    unfold le_aux at *, 
+    
+    simp at *,
+    -/
+end
+
+instance : is_monomial_order (ℕ →₀ ℕ) finsupp.le := sorry
+
+end finsupp
+
+
+def leading_term (p : mv_polynomial ℕ α) : mv_polynomial ℕ α := sorry
+
+def buchberger {s : set (mv_polynomial ℕ α)} : set (mv_polynomial ℕ α) := sorry
