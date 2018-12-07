3eb7573cdc8036c386ff570d19ce7fa5cb830c4b
petercommand <petercommand@gmail.com>
2018-12-07 21:29:33 +0800

    add leading_term

--- src/mv_polynomial.lean
+++ src/mv_polynomial.lean
@@ -2,13 +2,16 @@ import linear_algebra.multivariate_polynomial
 variables {α : Type*} {β : Type*} {γ : Type*} {δ : Type*} {ι : Type*}
 variables [discrete_field α]
 variables [decidable_eq α] [decidable_linear_order α]
-variables [lattice.semilattice_sup_bot (β →₀ ℕ)]
+variables [lattice.semilattice_sup_bot (β →₀ ℕ)] [decidable_eq (β →₀ ℕ)]
 namespace mv_polynomial
 
 section general
 
 def leading_term' (a: mv_polynomial β α): β →₀ ℕ := a.support.sup id
+def leading_term (a: mv_polynomial β α): mv_polynomial β α 
+    := finsupp.single (a.support.sup id) (a.to_fun (a.support.sup id))
 
+def leading_coeff (a: mv_polynomial β α): α := a.to_fun (a.support.sup id)
 end general
 
 end mv_polynomial
