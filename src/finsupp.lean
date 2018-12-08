fb4771d84da70da45a8f8055e164e07521f4f171
Han <potsrevennil@gmail.com>
2018-12-08 17:47:37 +0800

    finsupp.fin_n.le decidable_monomial_order

--- src/finsupp.lean
+++ src/finsupp.lean
@@ -60,6 +60,7 @@ begin
 end
 
 lemma coe_f : Π (a : α →₀ β) (n : α), a n = a.to_fun n := λ a n, rfl
+
 end has_zero
 
 section canonically_ordered_monoid
@@ -93,29 +94,131 @@ end
 end canonically_ordered_monoid
 end general
 
-section fin_n
+namespace fin_n
 variable {n : ℕ}
 include n
 
-def le_aux : ((fin $ n + 1) →₀ ℕ) → ((fin $ n + 1) →₀ ℕ) → ℕ → Prop
-| a b 0 := a 0 ≤ b 0
-| a b (m + 1) := a (m + 1) < b (m + 1) ∨ (a m = b m ∧ le_aux a b m)
+def le_aux : ∀ m < (n + 1), ((fin $ n + 1) →₀ ℕ) → ((fin $ n + 1) →₀ ℕ) → Prop
+| 0 h := λ a b, a ⟨0, h⟩ ≤ b ⟨0, h⟩
+| (m + 1) h := λ a b, a ⟨m + 1, h⟩ < b ⟨m + 1, h⟩ ∨ (a ⟨m + 1, h⟩ = b ⟨m + 1, h⟩ ∧ le_aux m (nat.lt_of_succ_lt h) a b)
 
-protected def le: rel ((fin $ n + 1) →₀ ℕ) := λ a b, le_aux a b n
+protected def le: rel ((fin $ n + 1) →₀ ℕ) := λ a b, le_aux n (nat.lt_succ_self n) a b 
 
-lemma le_refl_aux (m : ℕ): ∀ a : (fin $ n + 1) →₀ ℕ, le_aux a a m :=
-begin
-    intros, induction m,
-    unfold le_aux,
+lemma le_refl_aux (m : ℕ) (h : m < n + 1) : ∀ a : (fin $ n + 1) →₀ ℕ, le_aux m h a a :=
+λ a, begin
+    induction m;
     unfold le_aux, right,
-    apply and.intro, refl, exact m_ih,
+    apply and.intro rfl (m_ih (nat.lt_of_succ_lt h)),
 end
 
-lemma le_refl : ∀ a : (fin $ n + 1) →₀ ℕ, finsupp.le a a :=
-begin
-    intro a, unfold finsupp.le,
-    apply le_refl_aux,
+lemma le_refl : ∀ a : (fin $ n + 1) →₀ ℕ, finsupp.fin_n.le a a := 
+λ a, by apply le_refl_aux
+
+lemma le_trans_aux (m : ℕ) (h : m < n + 1) : ∀ a b c : (fin $ n + 1) →₀ ℕ, le_aux m h a b → le_aux m h b c → le_aux m h a c :=
+λ a b c, begin
+    induction m; unfold le_aux, apply le_trans,
+    intros hab hbc, cases hab; cases hbc,
+    left, exact lt_trans hab hbc,
+    left, exact lt_of_lt_of_le hab (le_of_eq hbc.left),
+    left, exact lt_of_le_of_lt (le_of_eq hab.left) hbc,
+    right, apply and.intro (eq.trans hab.left hbc.left) (m_ih (nat.lt_of_succ_lt h) hab.right hbc.right),
 end
+
+lemma le_trans : ∀ a b c : (fin $ n + 1) →₀ ℕ, 
+    finsupp.fin_n.le a b → finsupp.fin_n.le b c → finsupp.fin_n.le a c := 
+λ a b c, by apply le_trans_aux
+
+instance : preorder ((fin $ n + 1) →₀ ℕ) :=
+{
+    le := finsupp.fin_n.le,
+    le_refl := le_refl,
+    le_trans := le_trans,
+}
+
+lemma le_antisymm_aux (m₁ m₂ : ℕ) (h : m₁ + m₂ < n + 1) : ∀ a b : (fin $ n + 1) →₀ ℕ, 
+    le_aux (m₁ + m₂) h a b → le_aux (m₁ + m₂) h b a → a.to_fun ⟨m₁, nat.left_lt_of_add_lt h⟩ = b.to_fun ⟨m₁, nat.left_lt_of_add_lt h⟩ :=
+λ a b, begin
+    induction m₂, induction m₁, all_goals {simp [le_aux, nat.add_succ]}, 
+    apply le_antisymm,
+    all_goals {intros hab hba, cases hab; cases hba},
+    any_goals { 
+        try {apply absurd hab (not_lt_of_le (le_of_lt hba))},
+        try {apply absurd hab (not_lt_of_le (le_of_eq hba.left))},
+        try {apply absurd hba (not_lt_of_le (le_of_eq hab.left))},
+    },
+    exact hab.left,
+    rw [nat.add_succ] at h,
+    exact m₂_ih (nat.lt_of_succ_lt h) hab.right hba.right,
+end
+
+set_option trace.check true
+lemma le_antisymm : ∀ a b : (fin $ n + 1) →₀ ℕ, a ≤ b → b ≤ a → a = b := 
+λ a b, begin
+    intros hab hba,
+    apply ext, intro x, 
+    have h_add_sub : x.val + (n - x.val) = n, rw [←nat.add_sub_assoc (nat.le_of_lt_succ x.is_lt)], simp,
+    have h := le_antisymm_aux x.val (n - x.val) (lt_of_le_of_lt (le_of_eq h_add_sub) (nat.lt_succ_self n)) a b, 
+    simp [h_add_sub] at h,
+    apply h hab hba,
+end
+
+lemma le_total_aux (m : ℕ) (h : m < n + 1) : ∀ a b : (fin $ n + 1) →₀ ℕ, le_aux m h a b ∨ le_aux m h b a :=
+λ a b, begin
+    induction m; unfold le_aux, apply le_total,
+    cases lt_trichotomy (a.to_fun ⟨m_n + 1, h⟩) (b.to_fun ⟨m_n + 1, h⟩),
+    apply or.inl (or.inl h_1),
+    cases h_1, cases m_ih (nat.lt_of_succ_lt h),
+        apply or.inl (or.inr (and.intro h_1 h_2)),
+        apply or.inr (or.inr (and.intro h_1.symm h_2)),
+    apply or.inr (or.inl h_1),
+end
+
+lemma le_total : ∀ a b : (fin $ n + 1) →₀ ℕ, a ≤ b ∨ b ≤ a :=
+λ a b, by unfold has_le.le preorder.le; apply le_total_aux n
+
+instance decidable_le_aux (m : ℕ) (h : m < n + 1) : decidable_rel (le_aux m h) :=
+λ a b, begin
+    induction m;
+    unfold le_aux, apply_instance,
+    from if h₀ : (a.to_fun ⟨m_n + 1, h⟩) < (b.to_fun ⟨m_n + 1, h⟩)
+    then is_true (or.inl h₀)
+    else if h₁ : (a.to_fun ⟨m_n + 1, h⟩) > (b.to_fun ⟨m_n + 1, h⟩)
+    then is_false (λ h₁', begin 
+        cases h₁', apply nat.lt_lt_antisym h₁ h₁',
+        apply absurd h₁ (not_lt_of_le (le_of_eq h₁'.left)),
+    end)
+    else begin
+        cases m_ih (nat.lt_of_succ_lt h),
+        left, intro h_1', cases h_1', apply absurd h_1' h₀, apply absurd h_1'.right h_1,
+        right, apply or.inr (and.intro (nat.not_le_not_ge_eq h₀ h₁) h_1),
+    end
+end
+
+instance : decidable_rel ((≤) : rel ((fin $ n + 1) →₀ ℕ)) := λ a b,
+by unfold has_le.le preorder.le finsupp.fin_n.le; apply_instance
+
+lemma le_mono_order_aux (m : ℕ) (h : m < n + 1) : ∀ a b w : (fin $ n + 1) →₀ ℕ, le_aux m h a b →  le_aux m h (a + w) (b + w) :=
+λ a b w, begin
+    induction m;
+    unfold le_aux, simp,
+    intro hab, simp, 
+    cases hab, exact or.inl hab,
+    exact or.inr (and.intro hab.left (m_ih (nat.lt_of_succ_lt h) hab.right)),
+end
+
+lemma le_mono_order : ∀ (a b w : (fin $ n + 1) →₀ ℕ), (a ≤ b) → ((a + w) ≤ (b + w)) := 
+λ a b w, by apply le_mono_order_aux
+
+instance : decidable_monomial_order ((fin $ n + 1) →₀ ℕ) := {
+    le := preorder.le,
+    le_refl := preorder.le_refl,
+    le_trans := preorder.le_trans,
+    le_antisymm := le_antisymm,
+    le_total := le_total,
+    decidable_le := finsupp.fin_n.decidable_rel,
+    add := finsupp.has_add.add,
+    mono_order := le_mono_order,
+}
 
 omit n
 
