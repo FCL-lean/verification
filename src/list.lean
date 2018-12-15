d9600dd2b2a2642d9b534802298357363a352bf3
petercommand <petercommand@gmail.com>
2018-12-15 18:26:18 +0800

    remove using_well_founded in list.lean

--- src/list.lean
+++ src/list.lean
@@ -7,15 +7,9 @@ def fold' : Π {α : Type u}(C : list α → Sort v)
     C [] → 
     (Π (elem: α) (l₂: list α), elem ∈ l → l₂ ⊆ l → C l₂ → C (elem :: l₂)) → 
     C l
-| α C [] d ih := d
-| α C (x :: xs) d ih := 
-    have xs.length < 1 + xs.length := by rw [add_comm]; constructor,
+| α C [] := λ d ih, d
+| α C (x :: xs) := λ d ih,
     ih x xs (by simp) (by simp) 
         (fold' C xs d (λ elem l₂ inxs inxs2 Cl2, 
             (ih elem l₂ (by right; assumption) 
                 (list.subset_cons_of_subset _ inxs2) Cl2)))
-using_well_founded {
-    rel_tac :=
-    λ _ _, `[exact ⟨_, measure_wf (λ k, list.length k.2.2.1)⟩],
-    dec_tac := `[unfold has_well_founded.r inv_image measure, simp, assumption]   
-}
