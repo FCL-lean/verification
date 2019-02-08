de729f79451fa5de1b0e7b3315f4ec16a86264d5
petercommand <petercommand@gmail.com>
2019-02-08 22:01:28 +0800

    init commit

---
+++ src/polynomial.lean
@@ -0,0 +1,202 @@
+import data.list
+import data.finsupp
+import data.fin
+
+import util
+universes u v
+
+@[reducible]
+def monomial : ℕ → Type := λ n, fin n →₀ ℕ
+
+structure poly_term (α : Type u) (n : ℕ) [discrete_field α] :=
+mk :: (coeff : α) (mon : monomial n)
+
+
+variable {n : ℕ}
+
+
+
+lemma fin_0_id (a b : fin 0 →₀ ℕ) : a = b := begin apply finsupp.ext, intro x, cases x.2, end 
+
+def le_aux : ∀ m < (n + 1), ((fin $ n + 1) →₀ ℕ) → ((fin $ n + 1) →₀ ℕ) → Prop
+| 0 h := λ a b, a ⟨0, h⟩ ≤ b ⟨0, h⟩
+| (m + 1) h := λ a b, a ⟨m + 1, h⟩ < b ⟨m + 1, h⟩ ∨ (a ⟨m + 1, h⟩ = b ⟨m + 1, h⟩ ∧ le_aux m (nat.lt_of_succ_lt h) a b)
+
+def mon_le: Π {n}, rel (fin n →₀ ℕ)
+| 0 a b            := true
+| (nat.succ n) a b := le_aux n (nat.lt_succ_self n) a b
+
+lemma le_refl_aux (m : ℕ) (h : m < n + 1) : ∀ a : (fin $ n + 1) →₀ ℕ, le_aux m h a a :=
+λ a, begin
+    induction m;
+    unfold le_aux, right,
+    apply and.intro rfl (m_ih (nat.lt_of_succ_lt h)),
+end
+
+lemma mon_le_refl : ∀ a : fin n →₀ ℕ, mon_le a a := 
+λ a, by cases n; simp [mon_le]; apply le_refl_aux
+
+lemma le_trans_aux (m : ℕ) (h : m < n + 1) : ∀ a b c : (fin $ n + 1) →₀ ℕ, le_aux m h a b → le_aux m h b c → le_aux m h a c :=
+λ a b c, begin
+    induction m; unfold le_aux, apply le_trans,
+    intros hab hbc, cases hab; cases hbc,
+    left, exact lt_trans hab hbc,
+    left, exact lt_of_lt_of_le hab (le_of_eq hbc.left),
+    left, exact lt_of_le_of_lt (le_of_eq hab.left) hbc,
+    right, apply and.intro (eq.trans hab.left hbc.left) (m_ih (nat.lt_of_succ_lt h) hab.right hbc.right),
+end
+
+lemma mon_le_trans : ∀ a b c : fin n →₀ ℕ, 
+    mon_le a b → mon_le b c → mon_le a c := 
+λ a b c, by cases n; simp [mon_le]; apply le_trans_aux
+
+instance : preorder (fin n →₀ ℕ) :=
+{
+    le := mon_le,
+    le_refl := mon_le_refl,
+    le_trans := mon_le_trans,
+}
+
+
+lemma le_antisymm_aux (m₁ m₂ : ℕ) (h : m₁ + m₂ < n + 1) : ∀ a b : (fin $ n + 1) →₀ ℕ, 
+    le_aux (m₁ + m₂) h a b → le_aux (m₁ + m₂) h b a → a.to_fun ⟨m₁, nat.left_lt_of_add_lt h⟩ = b.to_fun ⟨m₁, nat.left_lt_of_add_lt h⟩ :=
+λ a b, begin
+    induction m₂, induction m₁, all_goals {simp [le_aux, nat.add_succ]}, 
+    apply le_antisymm,
+    all_goals {intros hab hba, cases hab; cases hba},
+    any_goals { 
+        try {apply absurd hab (not_lt_of_le (le_of_lt hba))},
+        try {apply absurd hab (not_lt_of_le (le_of_eq hba.left))},
+        try {apply absurd hba (not_lt_of_le (le_of_eq hab.left))},
+    },
+    exact hab.left,
+    rw [nat.add_succ] at h,
+    exact m₂_ih (nat.lt_of_succ_lt h) hab.right hba.right,
+end
+
+lemma mon_le_antisymm : ∀ a b : fin n →₀ ℕ, a ≤ b → b ≤ a → a = b := 
+λ a b, begin
+    cases n;
+    intros hab hba, 
+    apply fin_0_id,
+    apply finsupp.ext, intro x, 
+    have h_add_sub : x.val + (n - x.val) = n, rw [←nat.add_sub_assoc (nat.le_of_lt_succ x.is_lt)], simp,
+    have h := le_antisymm_aux x.val (n - x.val) (lt_of_le_of_lt (le_of_eq h_add_sub) (nat.lt_succ_self n)) a b, 
+    simp [h_add_sub] at h,
+    apply h hab hba,
+end
+
+lemma le_total_aux (m : ℕ) (h : m < n + 1) : ∀ a b : (fin $ n + 1) →₀ ℕ, le_aux m h a b ∨ le_aux m h b a :=
+λ a b, begin
+    induction m; unfold le_aux, apply le_total,
+    cases lt_trichotomy (a.to_fun ⟨m_n + 1, h⟩) (b.to_fun ⟨m_n + 1, h⟩),
+    apply or.inl (or.inl h_1),
+    cases h_1, cases m_ih (nat.lt_of_succ_lt h),
+        apply or.inl (or.inr (and.intro h_1 h_2)),
+        apply or.inr (or.inr (and.intro h_1.symm h_2)),
+    apply or.inr (or.inl h_1),
+end
+
+lemma mon_le_total : ∀ a b : fin n →₀ ℕ, a ≤ b ∨ b ≤ a :=
+λ a b, by cases n; simp [has_le.le, preorder.le, mon_le]; apply le_total_aux n
+
+instance decidable_le_aux (m : ℕ) (h : m < n + 1) : decidable_rel (le_aux m h) :=
+λ a b, begin
+    induction m;
+    unfold le_aux, apply_instance,
+    from if h₀ : (a.to_fun ⟨m_n + 1, h⟩) < (b.to_fun ⟨m_n + 1, h⟩)
+    then is_true (or.inl h₀)
+    else if h₁ : (a.to_fun ⟨m_n + 1, h⟩) > (b.to_fun ⟨m_n + 1, h⟩)
+    then is_false (λ h₁', begin 
+        cases h₁', apply nat.lt_lt_antisym h₁ h₁',
+        apply absurd h₁ (not_lt_of_le (le_of_eq h₁'.left)),
+    end)
+    else begin
+        cases m_ih (nat.lt_of_succ_lt h),
+        left, intro h_1', cases h_1', apply absurd h_1' h₀, apply absurd h_1'.right h_1,
+        right, apply or.inr (and.intro (nat.not_le_not_ge_eq h₀ h₁) h_1),
+    end
+end
+
+instance mon_le_decidable_rel : decidable_rel ((≤) : rel (fin n →₀ ℕ)) := λ a b,
+by cases n; unfold has_le.le preorder.le mon_le; apply_instance
+
+lemma le_mono_order_aux (m : ℕ) (h : m < n + 1) : ∀ a b w : (fin $ n + 1) →₀ ℕ, le_aux m h a b →  le_aux m h (a + w) (b + w) :=
+λ a b w, begin
+    induction m;
+    unfold le_aux, simp,
+    intro hab, simp, 
+    cases hab, exact or.inl hab,
+    exact or.inr (and.intro hab.left (m_ih (nat.lt_of_succ_lt h) hab.right)),
+end
+
+lemma le_mono_order : ∀ (a b w : fin n →₀ ℕ), (a ≤ b) → ((a + w) ≤ (b + w)) := 
+λ a b w, by cases n; simp [has_le.le, preorder.le, mon_le]; apply le_mono_order_aux
+
+instance : decidable_monomial_order (fin n →₀ ℕ) := {
+    le := preorder.le,
+    le_refl := preorder.le_refl,
+    le_trans := preorder.le_trans,
+    le_antisymm := mon_le_antisymm,
+    le_total := mon_le_total,
+    decidable_le := mon_le_decidable_rel,
+    add := finsupp.has_add.add,
+    mono_order := le_mono_order,
+}
+
+lemma zero_le_aux : ∀ (m < n + 1) (a : fin (n + 1) →₀ ℕ), le_aux m H 0 a
+| 0 H a := by simp [le_aux]
+|(m + 1) H a := by simp [le_aux]; 
+                from if h : 0 < a ⟨m + 1, H⟩ 
+                    then by simp [h]
+                    else begin exact or.inr (and.intro (nat.eq_zero_of_le_zero (le_of_not_lt h)).symm
+                                (zero_le_aux m (nat.lt_of_succ_lt H) a)) end
+
+lemma mon_zero_le : ∀ a : fin n →₀ ℕ, 0 ≤ a :=
+λ a, by cases n; simp [has_le.le, preorder.le, mon_le, zero_le_aux]
+
+
+inductive polynomial (α : Type u) (n : ℕ) [discrete_field α] : list (poly_term α n) → Type u
+| sing : Π (p : poly_term α n), polynomial [p]
+| cons : Π (p p': poly_term α n) (tail : list (poly_term α n)), p'.mon < p.mon → polynomial (p' :: tail) → polynomial (p :: p' :: tail)
+
+inductive polynomial' (α : Type u) (n : ℕ) [discrete_field α] : Type u
+| mk : Π (p : list (poly_term α n)) (poly : polynomial α n p), polynomial'
+
+def mon_insert {α : Type u} {n : ℕ} [discrete_field α] :
+    Π (t : poly_term α n) (l : list (poly_term α n)), list (poly_term α n)
+| t [] := [t]
+| t (x :: xs) := if p : x.2 ≤ t.2 then 
+                    (if p2 : t.2 = x.2 
+                     then poly_term.mk (t.1 + x.1) t.2 :: xs 
+                     else t :: x :: xs)
+                 else x :: mon_insert t xs
+
+def poly_add_insert {α : Type u} {n : ℕ} [discrete_field α] : 
+    Π (t : poly_term α n) (l : list (poly_term α n)), polynomial α n l → polynomial α n (mon_insert t l) :=
+begin
+    intros, induction l with hd tl l_ih,
+    { cases a },
+    unfold mon_insert,
+    by_cases mon_le hd.mon t.mon; simp [h],
+    { by_cases h_eq : t.mon = hd.mon, simp [h_eq],
+      { cases a; constructor; simpa, },
+      { replace h_eq : t.mon ≠ hd.mon := h_eq, simp [h_eq], 
+        constructor,
+        { exact lt_of_le_of_ne h h_eq.symm, },
+        { assumption },
+      }
+    },
+    { generalize l_h : list.cons hd tl = l,
+      rw l_h at a,
+      induction a; sorry,
+    }
+end
+
+def poly_add {α : Type u} {n : ℕ} [discrete_field α] : polynomial' α n → polynomial' α n → polynomial' α n
+| (polynomial'.mk [hd] poly) (polynomial'.mk l poly') := polynomial'.mk _ $ poly_add_insert hd l poly'
+| (polynomial'.mk (p :: p' :: tl) poly) (polynomial'.mk [hd] poly') := polynomial'.mk _ $ poly_add_insert hd _ poly
+| (polynomial'.mk (p :: p' :: tl) (polynomial.cons .(p) .(p') .(tl) pf poly)) 
+                             pp@(polynomial'.mk (p'' :: p''' :: tl') (polynomial.cons .(p'') .(p''') .(tl') pf' poly')) 
+      := poly_add (polynomial'.mk (p' :: tl) poly) $ polynomial'.mk (mon_insert p (p'' :: p''' :: tl')) (poly_add_insert p (p'' :: p''' :: tl') (by constructor; assumption))
+ 
---
+++ src/util.lean
@@ -0,0 +1,305 @@
+import data.set.lattice
+import data.finset
+import data.set
+
+universe u
+def not' : Type u → Prop := λ a, a → false
+notation `¬'` a := not' a
+
+inductive trans_closure {α : Sort u} (r : α → α → Prop) : α → α → Prop
+| base  : ∀ a b, r a b → trans_closure a b
+| trans : ∀ a b c, trans_closure a b → trans_closure b c → trans_closure a c
+
+inductive refl_trans_closure {α : Sort u} (r : α → α → Prop) : α → α → Prop
+| base  : ∀ a b, r a b → refl_trans_closure a b
+| refl  : ∀ a, refl_trans_closure a a
+| trans : ∀ a b c, refl_trans_closure a b → refl_trans_closure b c → refl_trans_closure a c
+
+
+
+section classes
+set_option old_structure_cmd true
+class is_monomial_order (α : Type u) (r : α → α → Prop) [has_add α] : Prop :=
+    (mono_order : ∀ a b w, r a b → r (a + w) (b + w))
+
+class monomial_order (α : Type*) extends has_add α, linear_order α := 
+    (mono_order : ∀ a b w : α, (a ≤ b) → (a + w) ≤ (b + w))
+
+class decidable_monomial_order (α : Type*) extends monomial_order α, decidable_linear_order α
+
+end classes
+
+namespace option
+variables {α : Type*}
+
+lemma some_lem {a : α} : (a : option α) = some a := by finish
+
+end option
+
+namespace nat
+
+lemma many_step 
+    : ∀ (P : ℕ → ℕ → Prop), (∀ n, P n (n + 1)) 
+        → is_trans ℕ P 
+        → ∀ n m, n < m → P n m :=
+begin
+    intros P p', introI, intros,
+    induction a_1,
+    by apply p',
+    begin
+        cases a,
+        apply a, exact a_1_ih, apply p',
+    end,
+end
+
+lemma many_step'
+    : ∀ (P : ℕ → ℕ → Prop), (∀ n, P n (n + 1))
+        → is_refl ℕ P
+        → is_trans ℕ P 
+        → ∀ n m, n ≤ m → P n m :=
+begin
+    intros P step,
+    introsI r t,
+    intros,
+    cases a,
+    by cases r; apply r,
+    begin
+        induction a,
+        by cases r; apply r,
+        begin
+            cases t,
+            apply t, exact a_ih, apply step,
+        end,
+    end,
+end
+
+
+
+lemma lt_lt_antisym : ∀ {a b : ℕ}, a < b → b < a → false
+| a b le₀ le₁ := begin apply (absurd le₀ (not_lt_of_ge (le_of_lt le₁))), end
+
+lemma left_lt_of_add_lt : ∀ {a b n : ℕ}, a + b < n → a < n :=
+λ a b n h, by apply lt_of_le_of_lt (nat.le_add_right a b) h
+
+lemma not_le_not_ge_eq {a b : ℕ} : ¬ a < b → ¬ b < a → a = b :=
+begin
+    intros,
+    have h := lt_trichotomy a b,
+    cases h, exact absurd h a_1,
+    cases h, assumption, exact absurd h a_2,
+end
+
+lemma sup_ab_eq_a_or_b : Π (a b : ℕ), a ⊔ b = a ∨ a ⊔ b = b
+| a b :=
+begin
+    intros,
+    by_cases (a ≤ b), right, apply lattice.sup_of_le_right, assumption,
+    by_cases (b ≤ a), left, apply lattice.sup_of_le_left, assumption,
+    rw not_le at *, apply false.elim, dedup, apply nat.lt_lt_antisym h h_1,
+end
+
+lemma zero_of_add {a b : ℕ} : a + b = a → b = 0 :=
+λ h, if h₁ : b > 0 
+    then begin 
+        have h₂ : a + b > a := nat.lt_add_of_pos_right h₁, rw h at h₂,
+        apply absurd h₂ (not_lt_of_le (le_of_eq rfl)),
+    end
+    else by simp at h₁; assumption
+
+lemma le_add {a b : ℕ}: a ≤ b → Σ' (i : ℕ), a + i = b :=
+begin
+    revert a,
+    induction b;
+    intros,
+    begin
+        apply psigma.mk 0,
+        exact nat.le_zero_iff.1 a_1,
+    end,
+    begin
+        cases a,
+        begin
+            apply psigma.mk (succ b_n),
+            simp,
+        end,
+        begin
+            have ih := b_ih (le_of_succ_le_succ a_1),
+            cases ih,
+            apply psigma.mk ih_fst,
+            simp, apply congr_arg nat.succ,
+            rwa [add_comm],
+        end,
+    end,
+end
+lemma lt_add {a b : ℕ}: a < b → Σ' (i : ℕ), a + nat.succ i = b :=
+begin
+    intro lt,
+    have lee := le_add lt,
+    cases lee,
+    apply psigma.mk lee_fst,
+    rw [←lee_snd], simp, apply congr_arg nat.succ,
+    simp,
+end
+
+end nat
+
+namespace logic
+
+lemma demorgan : Π {a b : Prop}, ¬ (a ∨ b) → ¬ a ∧ ¬ b :=
+begin
+    intros, constructor,
+    intro, apply a_1, left, assumption,
+    intro, apply a_1, right, assumption,
+end
+
+lemma dite_true {c : Prop} [h: decidable c] {α : Sort u} {b₁ : c → α} {b₂ : ¬ c → α}
+    : Π (ev: c = true), dite c b₁ b₂ = b₁ (@eq.subst _ id _ _ (eq.symm ev) true.intro)
+ :=
+begin
+    intros,
+    unfold dite, tactic.unfreeze_local_instances, cases h;
+    simp, apply false.elim, apply h, rw ev, trivial,
+    trivial,
+end
+
+lemma ite_true {c : Prop} [h: decidable c] {α : Sort u} {b₁ b₂ : α}
+    : Π (ev: c = true), ite c b₁ b₂ = b₁ :=
+begin
+    intros, unfold ite, 
+    tactic.unfreeze_local_instances,
+    cases h, apply false.elim, apply h, rw ev, trivial,
+    trivial,
+end
+
+lemma dite_true' {c : Prop} [h: decidable c] {α : Sort u} {b₁ : c → α} {b₂ : ¬ c → α}
+    : Π ev: c, dite c b₁ b₂ = b₁ ev
+ :=
+begin
+    intros, unfold dite,
+    tactic.unfreeze_local_instances,
+    cases h, apply false.elim, apply h, assumption,
+    trivial,
+end
+
+lemma ite_true' {c : Prop} [h: decidable c] {α : Sort u} {b₁ b₂ : α}
+    : Π (ev: c), ite c b₁ b₂ = b₁ :=
+begin
+    intros, unfold ite, 
+    tactic.unfreeze_local_instances,
+    cases h, apply false.elim, apply h, assumption,
+    trivial,
+end
+
+lemma dite_false {c : Prop} [h: decidable c] {α : Sort u} {b₁ : c → α} {b₂ : ¬ c → α}
+    : Π (ev: c = false), dite c b₁ b₂ = b₂ (λ neq, begin rw ev at neq, assumption end)
+ :=
+begin
+    intros, unfold dite,
+    tactic.unfreeze_local_instances, cases h; simp,
+    apply false.elim, rw ev at h, assumption,
+end
+
+lemma ite_false {c : Prop} [h: decidable c] {α : Sort u} {b₁ b₂ : α}
+    : Π (ev: c = false), ite c b₁ b₂ = b₂ :=
+begin
+    intros, unfold ite, 
+    tactic.unfreeze_local_instances,
+    cases h, simp,
+    apply false.elim, rw ev at *, assumption,
+end
+
+
+lemma dite_false' {c : Prop} [h: decidable c] {α : Sort u} {b₁ : c → α} {b₂ : ¬ c → α}
+    : Π ev: ¬ c, dite c b₁ b₂ = b₂ ev
+ :=
+begin
+    intros, unfold dite,
+    tactic.unfreeze_local_instances,
+    cases h,
+    simp, apply false.elim, apply ev, assumption,
+end
+
+lemma ite_false' {c : Prop} [h: decidable c] {α : Sort u} {b₁ b₂ : α}
+    : Π (ev: ¬ c), ite c b₁ b₂ = b₂ :=
+begin
+    intros, unfold ite, 
+    tactic.unfreeze_local_instances,
+    cases h, simp,
+    apply false.elim, apply ev, assumption,
+end
+
+lemma or_elim' {a b : Prop} [decidable a] [decidable b] {c : Type*} : (a ∨ b) → (a → c) → (b → c) → c :=
+λ h hac hbc, begin
+    by_cases ha : a, exact hac ha,
+    apply hbc,
+    cases h, apply absurd h ha, assumption,
+end
+
+end logic
+universe α
+
+def rel (A : Sort α) := A → A → Prop
+
+namespace list
+variables {α : Type*} [decidable_eq α]
+
+lemma mem_to_set {a : α} {l : list α} : a ∈ l ↔ a ∈ l.to_finset.to_set := 
+begin
+    apply iff.intro; intro h, rw [←list.mem_to_finset, ←finset.mem_coe] at h, assumption,
+    have h' : a ∈ l.to_finset, rw [←finset.mem_coe], exact h,
+    simp at h', assumption,
+end
+
+lemma subset_to_finset {l₁ l₂ : list α} : l₁ ⊆ l₂ ↔ l₁.to_finset ⊆ l₂.to_finset :=
+begin
+    apply iff.intro; unfold has_subset.subset list.subset;
+    intros h a ha, 
+    rw mem_to_finset at *, exact h ha,
+    rw ←mem_to_finset at *, exact h ha,
+end
+
+lemma subset_to_set {l₁ l₂ : list α} : l₁ ⊆ l₂ ↔ l₁.to_finset.to_set ⊆ l₂.to_finset.to_set :=
+begin
+    apply iff.intro; unfold has_subset.subset list.subset set.subset;
+    intros h a ha, 
+    rw ←mem_to_set at *, exact h ha,
+    rw mem_to_set at *, exact h ha,
+end
+
+lemma last_mem (α : Sort*): Π (l : list α) (h : l ≠ list.nil),  
+        l.last h ∈ l :=
+begin
+    intro l,
+    induction l;
+    intros, apply false.elim, apply h, trivial,
+    simp, cases l_tl,
+    left, simp,
+    right, simp,
+    apply l_ih,
+end
+
+lemma cons_iff_ne_nil : ∀ (l : list α), l ≠ [] ↔ ∃ hd tl, l = hd :: tl
+| [] := ⟨λ h, by finish, λ h, by finish⟩
+| (hd :: tl) := ⟨λ h, begin refine ⟨hd, ⟨tl, _⟩⟩, refl, end, 
+    λ h, by simp⟩
+
+lemma exists_mem_iff_ne_nil : ∀ (l : list α), l ≠ [] ↔ ∃ a, a ∈ l :=
+λ l, begin
+    rw [cons_iff_ne_nil], split; intro h,
+    rcases h with ⟨hd, ⟨tl, h⟩⟩, use hd, simp [h],
+    rw ←cons_iff_ne_nil,
+    cases h,
+    apply ne_nil_of_mem h_h,
+end
+
+lemma foldl_eq {β : Type*} : ∀ (l : list α) {a b : β} (f : β → α → β), a = b → l.foldl f a = l.foldl f b :=
+λ l a b f h, by simp [h]
+
+end list
+
+namespace set
+variables {α : Type*}
+
+end set
+
+lemma eq.subst' {α : Sort u} {a b : α} {P : Π (m: α), (m = a) ∨ (m = b) → Prop} 
+    (h₁ : a = b) (h₂ : P a (or.inl rfl)) : P b (or.inr rfl) := by cases h₁; assumption
