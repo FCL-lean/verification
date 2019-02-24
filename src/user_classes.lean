13af82522d150e474bc0a596a26b28f238dd2850
Han <potsrevennil@gmail.com>
2019-02-24 16:26:31 +0800

    Remove partial_order instance from proving zero_le'

--- src/user_classes.lean
+++ src/user_classes.lean
@@ -4,8 +4,8 @@ set_option old_structure_cmd true
 
 class decidable_canonically_ordered_monoid (α : Type*) extends canonically_ordered_monoid α, decidable_linear_ordered_cancel_comm_monoid α
 
-class is_monomial_order (α : Type*) [has_add α] [has_le α] :=
-(monomial_order : ∀ a b : α, a ≤ b → ∀ c : α, c + a ≤ c + b)
+class is_monomial_order (α : Type*) [has_add α] (r : α → α → Prop) :=
+(monomial_order : ∀ a b : α, r a b → ∀ c : α, r (c + a) (c + b))
 
 end old_structure_cmd
 
